611f8189e982687b42bd8a6e271aa2a33f1d3bc9
root <root@tfg07.virtual.lab.inf.uva.es>
2018-05-07 09:34:54 +0200

    createdb

--- createDB.js
+++ createDB.js
@@ -7,15 +7,15 @@ console.log("Primero eliminamos la base de datos");
 
 database.drop();
 
-console.log("En 5 segundos se crearán las tablas en la base de datos");
+console.log("En 3segundos se crearán las tablas en la base de datos");
 
 setTimeout(()=>{
   database.sync();
-  console.log("En 5 segundos se inicializarán las tablas con datos de ejemplo");
-},5000);
+  console.log("En 3 segundos se inicializarán las tablas con datos de ejemplo");
+},3000);
 
 setTimeout(()=>{
   const models = require('./models/filldb/fillModels');
   console.log("Instalación finalizada");
   console.log("Puede arrancar la aplicación");
-},10000);
+},1000);
