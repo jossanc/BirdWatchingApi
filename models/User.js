051fd6b0011a7b40375186246f7d62f0077dddd3
Jose Luis <jose_luis_sp_93@hotmail.com>
2018-05-07 18:26:56 +0200

    fk4

--- models/User.js
+++ models/User.js
@@ -11,9 +11,9 @@ const User = database.define('users', {
           // This is a reference to another model
           model: Area,
                // This is the column name of the referenced model
-          key: 'areaName',
+          key: 'areaName'//,
                // This declares when to check the foreign key constraint. PostgreSQL only.
-          deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+         // deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
         }
     }
 }, { createdAt: false, updatedAt: false });
