ded48d24ddd8915c57bca26c91b95ba113090f36
Jose Luis <jose_luis_sp_93@hotmail.com>
2018-05-10 18:17:38 +0200

    post user

--- routes/Users.js
+++ routes/Users.js
@@ -31,6 +31,25 @@ router.get('/:username', function(req,res){
   res.status(403).send({error:err.toString()});
 })});
 
+router.post('/', (req, res) => {
+    //Creates a new user
+    console.log(req);
+    if (req.body.userName === undefined ||
+        req.body.password === undefined ||
+        req.body.areaName === undefined ) {
+        res.sendStatus(412);
+        return;
+    }
+    var data = {};
+    data.userName = req.body.userName;
+    data.password = req.body.password;
+    data.areaName = req.body.areaName;
+
+    User.create(data)
+        .then((ok) => res.send(ok))
+        .catch((err) => res.sendStatus(500));
+});
+
 module.exports = {
     User: router
 }
